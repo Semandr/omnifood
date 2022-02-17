c9f5bd9df9a0264b91f7a480c77c3717f6cdbe3f
Semandr <semandr.code@gmail.com>
2022-02-17 13:06:28 +0100

    implementing a sticky navigation bar

--- js/app.js
+++ js/app.js
@@ -34,3 +34,24 @@ allLinks.forEach(function (link) {
     }
   });
 });
+
+// Sticky navigation
+const sectionHeroEl = document.querySelector(".section-hero");
+const observer = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+    if (!ent.isIntersecting) {
+      document.body.classList.add("sticky");
+    } else {
+      document.body.classList.remove("sticky");
+    }
+  },
+  {
+    // In the viewport, in the inside intire browser window
+    root: null, // where element must upparing or not
+    // It means when the "hero-section" is in 0% on the viewport
+    threshold: 0,
+    rootMargin: "-80px",
+  }
+);
+observer.observe(sectionHeroEl);
